6bf5124103d9778a1fd0b30f2070701d9a1c8e03
Asad <asadakmal11@icloud.com>
2025-04-26 14:17:22 +0500

    data imported and libraries imported

--- Phone-Dash.py
+++ Phone-Dash.py
@@ -1,1 +1,8 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import streamlit as st
+data = "C:\\Users\\USER\\OneDrive\\Desktop\\2nd Semester\\Ai Lab\\Phone-Dash\\Mobile phone price.csv"
+
+df=pd.read_csv(data)
+print(df)
+print(df['Brand'])
